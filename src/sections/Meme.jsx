d020c7c80850b91aaea780a61ed08aaa05e1cf9f
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-15 14:28:28 +0530

    Create Meme.jsx

---
+++ src/sections/Meme.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Meme() {
+  return (
+    <div  className=' container-wrapper py-4'>
+
+        <h2>
+        A Meme Saga begins...
+
+        </h2>
+
+        <div className=' flex items-center gap-4 justify-between flex-col sm:flex-row'>
+
+            <img src="" alt="" />
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Meme
