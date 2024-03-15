1bc127bfd77890cfbf810743d410d7ff94e3733a
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-15 14:33:11 +0530

    new

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,39 +1,38 @@
 import Navbar from "./components/Navbar";
 import AboutUs from "./sections/AboutUs";
 import Hero from "./sections/Hero";
-
-
+import Meme from "./sections/Meme";
 
 function AppLayout() {
-   return (
-      <>
-         
-         <main className="pb-5 pt-2 bg-main">
-         <Navbar />
-            <Hero />
-       
+  return (
+    <>
+      <main className="pb-5 pt-2 bg-main">
+        <Navbar />
+        <Hero />
+      </main>
+      <div>
+        <AboutUs />
 
-          
-         </main>
-         <div>
-               <AboutUs/>
+        <div className=" mt-4">
 
-               <div className="container-wrapper mt-8">
-               <div className=" flex items-center justify-center gap-2">
-       <img
-          src="/images/logo.png"
-          className="max-w-[7rem] w-full rounded-full"
-          alt=""
-        />
+         <Meme/>
 
-        <p className=" text-2xl font-medium">KingPepe</p>
-       </div>
+        </div>
 
-            </div>
-            </div>
+        <div className="container-wrapper mt-8">
+          <div className=" flex items-center justify-center gap-2">
+            <img
+              src="/images/logo.png"
+              className="max-w-[7rem] w-full rounded-full"
+              alt=""
+            />
 
-      </>
-   );
+            <p className=" text-2xl font-medium">KingPepe</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AppLayout;
--- src/sections/Meme.jsx
+++ src/sections/Meme.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 function Meme() {
   return (
-    <div  className=' container-wrapper py-4'>
+    <div  className=' container-wrapper py-4 px-4 mx-auto'>
 
-        <h2>
+        <h2 className=' text-2xl py-2'>
         A Meme Saga begins...
 
         </h2>
 
         <div className=' flex items-center gap-4 justify-between flex-col sm:flex-row'>
 
-            <img src="" alt="" />
-
+            <img src="images/meme1.jpeg" alt="" className=' rounded-xl border border-[#fed403] h-[350px]' />
+            <img src="images/meme2.jpeg" alt="" className=' rounded-xl border border-[#fed403] h-[350px]' />
+            <img src="images/meme3.jpeg" alt="" className=' rounded-xl border border-[#fed403] h-[350px]' />
+           
         </div>
 
     </div>
