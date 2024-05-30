e62643cc212b43c699efec3d2569f31536260d62
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-05-31 01:58:30 +0530

    Update Roadmap.jsx

--- src/sections/Roadmap.jsx
+++ src/sections/Roadmap.jsx
@@ -4,7 +4,7 @@ function Roadmap() {
       <div className=" container-wrapper py-1 px-4 mx-auto">
         <h1 className="text-5xl text-center py-6">Roadmap</h1>
 
-        <div className=" py-1 flex justify-center gap-8  bg-[#113a00] items-center px-4 border-[#a90303] border rounded-2xl">
+        <div className=" py-1 flex justify-center gap-8  bg-[#113a00] items-center px-4 border-[#a90303] border rounded-2xl flex-col sm:flex-row">
           <div>
             <h2>Phase 1</h2>
 
